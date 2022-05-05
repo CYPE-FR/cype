f8be47076518b67a56c3f0bf9fddc61c4b7f23f0
mabatan <thonyabatan@gmail.com>
2022-05-05 17:10:53 +0200

    cm

--- mail.php
+++ mail.php
@@ -2,15 +2,15 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$phone= $_POST['phone'];
 $message= $_POST['message'];
 $to = "thonyabatan@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "Phone". $phone. "\r\n Message =" . $message;
+$headers = "From: ".$email;
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
 header("Location:thankyou.html");
-
 ?>
