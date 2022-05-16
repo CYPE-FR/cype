1a5d0f584ec134e0059e8f0d1aba95c875a73d8f
mabatan <thonyabatan@gmail.com>
2022-05-16 09:51:31 +0200

    cm

--- contact.php
+++ contact.php
@@ -1,42 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Contact</title>
-    <link rel="stylesheet" href="contact.css">
-</head>
-<body>
-    <h1>Contactez-nous</h1>
-    <p>Pour toute demande, remplissez le formuliare ci-dessous.</p>
-    <form method="post">
-        <input type="text" name="nom" placeholder="Nom" required>
-        <small class="error"></small>
-
-        <input type="email" name="email" placeholder="Email" required>
-        <small class="error"></small>
-
-        <input type="text" name="phone" placeholder="Téléphone" required>
-        <small class="error"></small>
-
-        <input type="text" name="sujet" placeholder="Sujet" required>
-        <small class="error"></small>
-
-        <textarea name="message" placeholder="Message" required></textarea>
-        <input type="submit" value="Envoyer le message">
-        <small class="error"></small>
-
-    </form>
-    <?php
-
+<?php
     //si le formulaire a été soumis
-    if(isset($_POST["message"])){ 
+    if(isset($_POST["p_message"])){ 
         $message = "Ce message vous a été envoyé via la page contact du site vitrine 
-        Nom : " . $_POST["nom"] . "
-        Email : " . $_POST["email"] . "
-        Téléphone : " . $_POST["phone"] . "
+        Nom : " . $_POST["p_name"] . "
+        Email : " . $_POST["p_email"] . "
+        Sujet : " . $_POST["p_subject"] . "
+        Téléphone : " . $_POST["p_phone"] . "
         Message : " . $_POST["message"];
 
-        $retour = mail("thonyabatan@gmail.com",$_POST["sujet"],$message,"From:".$_POST["email"]."\r\n"."Reply-to:".$_POST["email"]);
+        $retour = mail("thonyabatan@gmail.com",$_POST["p_subject"],$message,"From:".$_POST["p_email"]."\r\n"."Reply-to:".$_POST["p_email"]);
         if($retour){
             echo"<p>L'email a bien été envoyé.<p>";
         }
@@ -44,5 +16,3 @@
    
     ?>
 </body>
-</html>
-
--- mail.php
+++ mail.php
@@ -1,16 +1,15 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$phone= $_POST['phone'];
-$message= $_POST['message'];
+$name = $_POST['p_name'];
+$email= $_POST['p_email'];
+$sujet= $_POST['p_subject'];
+$phone= $_POST['p_phone'];
+$message= $_POST['p_message'];
 $to = "thonyabatan@gmail.com";
 $subject = "Mail From website";
-$txt = " Name : ". $name . "\r\n Email : " . $email . "\r\n Phone : ". $phone. "\r\n Message : " . $message;
+$txt = " Name : ". $name . "\r\n Email : " . $email . "\r\n Phone : ". $phone. "\r\n Sujet : ". $sujet. "\r\n Message : " . $message;
 $headers = "From: ".$email;
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
-//redirect
-header("Location:thankyou.html");
 ?>
